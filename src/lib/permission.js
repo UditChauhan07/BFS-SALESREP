6abbc8a2bc9f25cef6f33f5fca1d02be77e7b0a6
UditChauhan07 <uditchauhan.dx@gmail.com>
2024-09-23 11:39:34 +0530

    update contact report

--- src/lib/permission.js
+++ src/lib/permission.js
@@ -171,7 +171,7 @@ export const permissionsArray = [
             view: true, //check
           },
           contactDetailedReport: {
-            view: false, //off
+            view: true, //off
           },
           accountTier: {
             view: false, //check
@@ -262,7 +262,7 @@ export const permissionsArray = [
             view: false, //check
           },
           contactDetailedReport: {
-            view: false, //off
+            view: true , //off
           },
           accountTier: {
             view: false, //check
